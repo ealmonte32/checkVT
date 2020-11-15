bd6b6625c4088e7ed3c1ab863514a077524e8f1d
Emyll Almonte <eg0dz@aol.com>
2020-11-14 22:20:21 -0500

    Add files via upload

--- faq.php
+++ faq.php
@@ -3,21 +3,20 @@ header("X-Frame-Options: SAMEORIGIN");
 header("X-XSS-Protection: 1");
 header("X-Content-Type-Options: nosniff");
 require_once('checkvt_priv/database.php');
-
 ?>
 <!DOCTYPE html>
 <html>
 <head>
+<meta charset="utf-8">
 <title>checkVT FAQ</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" type="text/css" href="style.css" />
+<link rel="stylesheet" type="text/css" href="style.css">
 <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 <link rel="icon" href="favicon.ico" type="image/x-icon">
 </head>
+</head>
 <body>
-
 <center>
-<br>
 <h2>checkVT Frequently Asked Questions</h2>
 <hr style="width: 320px; border: 1px dashed white">
 <br>
@@ -32,11 +31,10 @@ require_once('checkvt_priv/database.php');
 <li>{Q} Does this website/tool collect any personal information from my computer?</li>
 {A} Nope, this simply collects the original URL you entered and the final destination URL for research purposes and statistical reporting.
 </ul>
-<br>
-
 <br><br><br>
 <a href=".">Back</a><br>
 <b>checkVT version 1.0.3</b>
+<br>
 </center>
 </body>
 </html>
--- index.php
+++ index.php
@@ -3,27 +3,25 @@ header("X-Frame-Options: SAMEORIGIN");
 header("X-XSS-Protection: 1");
 header("X-Content-Type-Options: nosniff");
 require_once('checkvt_priv/database.php');
-
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-<title>checkVT process</title>
+<meta charset="utf-8">
+<title>checkVT</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" type="text/css" href="style.css" />
+<link rel="stylesheet" type="text/css" href="style.css">
 <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 <link rel="icon" href="favicon.ico" type="image/x-icon">
 </head>
 <body>
-
 <center>
-<br>
 <h2>checkVT processing...</h2>
 <hr style="width: 320px; border: 1px dashed white">
 <br>
 <div style="width:85%;">
 This page is used as the "processing" page because the whole process happens behind the scenes in php/curl functions.<br>
-We have this function accept the URL and performs a parsing process on it, then sends you to the VirusTotal destination.<br>
+These functions accept the URL and perform a parsing process on it, then sends you to the VirusTotal destination.<br>
 The browser extension does this automatically for you, but you can also manually enter a URL below to use this tool.
 </div>
 <br>
@@ -31,12 +29,12 @@ The browser extension does this automatically for you, but you can also manually
 <p>
 Enter the URL to process:
 <br><input type="text" name="incoming_url" size="40" autofocus required><br>
-<br><input type="submit" value="Submit"> <input type="reset" value="Clear">
+<br><input type="submit" class="button" value="Submit"> <input type="reset" class="button" value="Clear">
 </form>
-
-<br><br><br>
+<br>
 <a href="faq.php">FAQ</a><br>
 <b>checkVT version 1.0.3</b>
+<br><br>
 </center>
 </body>
 </html>

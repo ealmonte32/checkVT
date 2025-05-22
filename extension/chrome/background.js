ef7dbe0c259619203ed6cda01c097acd98aa761c
Emyll Almonte <eg0dz@aol.com>
2025-05-22 00:48:14 -0400

    Add files via upload

--- extension/chrome/background.js
+++ extension/chrome/background.js
@@ -1,45 +1,63 @@
 // ealmonte32
-// v:chrome
+// v:google.chrome
 
+// declare variables
+var google_query;
+var parsed_url;
+var parsed_url_query;
+var uri;
+var uri_decode;
+var url_scheme;
+var url_address;
+var url_query_match;
+
+// main
 chrome.contextMenus.create({
-	"id": "id_checkVT",
-	"title": "Send to checkVT",
-	"contexts": ["selection", "link"]
+    "id": "id_checkVT",
+    "title": "Send to checkVT",
+    "contexts": ["selection", "link"]
 });
 
-chrome.contextMenus.onClicked.addListener(function(item, tab) {
-"use strict";
+chrome.contextMenus.onClicked.addListener(function (item, tab) {
+    "use strict";
 
-  // if both a link and plaintext selection were detected, we remove the plaintext
-  if ((item.linkUrl) && (item.selectionText)){
-    delete item.selectionText;
-	}
-  
-  // if the item right clicked is plaintext
-  if (item.selectionText){
-  	let uri = item.selectionText;
-  	let uri_decode = decodeURIComponent(uri); //decode URI
-  	item.selectionText = uri_decode;
-  	
-  	// if the selected text does not contain an http/https scheme, add one
-  	if (!/^http:\/\//.test(item.selectionText) && !/^https:\/\//.test(item.selectionText)){
-  	  let url_scheme = 'http://';
-  	  let parsed_url = url_scheme.concat(item.selectionText);
-  	  let url = 'http://checkvt.epizy.com/checkvtprocess.php?incoming_url=' + parsed_url;
-	  chrome.tabs.create({url: url, index: tab.index + 1});
-  	} else {
-  	let url = 'http://checkvt.epizy.com/checkvtprocess.php?incoming_url=' + item.selectionText;
-  	chrome.tabs.create({url: url, index: tab.index + 1});
-  	}
-  }
+    // if both a link and plaintext selection were detected, we remove the plaintext
+    if ((item.linkUrl) && (item.selectionText)) {
+        delete item.selectionText;
+    }
 
-  // if the item right clicked is a link
-  if (item.linkUrl){
-  	let uri = item.linkUrl;
-  	let uri_decode = decodeURIComponent(uri); //decode URI
-  	item.linkUrl = uri_decode;
-	let url = 'http://checkvt.epizy.com/checkvtprocess.php?incoming_url=' + item.linkUrl;
-	chrome.tabs.create({url: url, index: tab.index + 1});
-	}
+    // if the item right clicked is plaintext
+    if (item.selectionText) {
+        uri = item.selectionText;
+        uri_decode = decodeURIComponent(uri); //decode URI
+        item.selectionText = uri_decode;
 
+        // if the selected text does not contain an http/https scheme, add one
+        if (!(/^http:\/\//.test(item.selectionText)) && !(/^https:\/\//.test(item.selectionText))) {
+            url_scheme = 'http://';
+            parsed_url = url_scheme.concat(item.selectionText);
+            url_address = 'https://checkvt.epizy.com/checkvtprocess.php?incoming_url=' + parsed_url;
+            chrome.tabs.create({url: url_address, index: tab.index + 1});
+        } else {
+            url_address = 'https://checkvt.epizy.com/checkvtprocess.php?incoming_url=' + item.selectionText;
+            chrome.tabs.create({url: url_address, index: tab.index + 1});
+        }
+    }
+
+    // if the item right clicked is a link
+    if (item.linkUrl) {
+        // if url is a google.com search query, take the url= part only
+        if ((/^https:\/\/www.google.com(.*)/.test(item.linkUrl)) && ((item.linkUrl).toString().match(/url\=(.*)/g))) {
+            google_query = item.linkUrl;
+            url_query_match = google_query.toString().match(/url\=(.*)/g);
+            parsed_url_query = url_query_match.toString().replace('url=', '');
+            item.linkUrl = encodeURIComponent(parsed_url_query); //encode URI
+            url_address = 'https://checkvt.epizy.com/checkvtprocess.php?incoming_url=' + item.linkUrl;
+            chrome.tabs.create({url: url_address, index: tab.index + 1});
+        } else {
+            item.linkUrl = encodeURIComponent(item.linkUrl); //encode URI
+            url_address = 'https://checkvt.epizy.com/checkvtprocess.php?incoming_url=' + item.linkUrl;
+            chrome.tabs.create({url: url_address, index: tab.index + 1});
+        }
+    }
 });
